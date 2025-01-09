a8f86e52a7df04cb40e094bcb6359f952e310bac
backend <shubhamprabhu21@gmail.com>
2025-01-10 00:32:06 +0530

    new Commit

---
+++ apps/api/src/models/users/rest/entity/user.entity.ts
@@ -0,0 +1,10 @@
+import { User } from '@prisma/client'
+import { IsDate, IsString, IsInt } from 'class-validator'
+import { RestrictProperties } from 'src/common/dtos/common.input'
+
+export class UserEntity implements RestrictProperties<UserEntity, User> {
+  name: string
+  uid: string
+  createdAt: Date
+  updatedAt: Date
+}
